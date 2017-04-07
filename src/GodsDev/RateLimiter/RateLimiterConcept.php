dcb522a0ddfa30d5f0bdc2c8687c7c7341c100d4
tomaskraus <tomas.krausik@gmail.com>
2017-04-07 06:12:59 +0200

    working implementation (dummy)

--- src/GodsDev/RateLimiter/RateLimiterConcept.php
+++ src/GodsDev/RateLimiter/RateLimiterConcept.php
@@ -17,9 +17,11 @@ class RateLimiterConcept implements \GodsDev\RateLimiter\RateLimiterInterface {
     public function __construct($rate, $period) {
         $this->rate = $rate;
         $this->period = $period;
+        $this->reset();
     }
 
     public function getHits($timestamp = null) {
+        $this->refreshState($timestamp);
         return $this->hits;
     }
 
@@ -32,16 +34,19 @@ class RateLimiterConcept implements \GodsDev\RateLimiter\RateLimiterInterface {
     }
 
     private function refreshState($timestamp) {
-        if ($timestamp == null) {
+        if (is_null($timestamp)) {
             $timestamp = time();
         }
-        $this->timeToWait = $this->period - ($timestamp - $this->startTime);
-        if ($this->timeToWait <= 0) {
-            $this->timeToWait = 0;
-            $this->hits = 0;
-            $this->startTime = $timestamp;
+        if ($timestamp - $this->startTime >= $this->period) {
+            //a new, clean period
+            $this->reset($timestamp);
+            //echo("**clean period  [(start=$this->startTime)]**");
         } else if ($this->hits < $this->rate) {
             $this->timeToWait = 0;
+            //echo("**hits<rate  [(start=$this->startTime) $this->hits<$this->rate]**");
+        } else {
+            $this->timeToWait = intval( ceil($this->period - ($timestamp - $this->startTime)) );
+            //echo("**time to wait [(per=$this->period, ts=$timestamp, start=$this->startTime) $this->timeToWait]**");
         }
     }
 
@@ -61,8 +66,16 @@ class RateLimiterConcept implements \GodsDev\RateLimiter\RateLimiterInterface {
     }
 
     public function reset($timestamp = null) {
-        $this->startTime = $timestamp;
+        if (is_null($timestamp)) {
+            $this->startTime = time();
+            //echo("^^C startTime reset to [$this->startTime] ^^");
+        } else {
+            $this->startTime = $timestamp;
+            //echo("^^C startTime reset to preset [$this->startTime] ^^");
+        }
+
         $this->hits = 0;
+        $this->timeToWait = 0;
     }
 
 }
--- test/GodsDev/RateLimiter/RateLimiterConceptTest.php
+++ test/GodsDev/RateLimiter/RateLimiterConceptTest.php
@@ -13,26 +13,24 @@ use GodsDev\RateLimiter\RateLimiterInterfaceTest;
  * @author Tomáš
  */
 class RateLimiterConceptTest extends RateLimiterInterfaceTest {
-    private $limiter;
-    private $limiterWrapper;
+
+
+    public function createLimiterTimeWrapper(\GodsDev\RateLimiter\RateLimiterInterface $rateLimiter) {
+        //return new LimiterTimeWrapper($rateLimiter, true); //real time wait
+        return new LimiterTimeWrapper($rateLimiter, false);
+    }
+
+    public function createRateLimiter() {
+        //return new \GodsDev\RateLimiter\RateLimiterConcept(10000, 3600*24); //10k/day
+        return new \GodsDev\RateLimiter\RateLimiterConcept(10, 5);
+    }
 
     protected function setUp() {
         parent::setUp();
-
-        $this->limiter = new RateLimiterConcept(10, 5);
-        $this->limiterWrapper = new LimiterTimeWrapper($this->limiter, true);
-
-        $this->limiterWrapper->reset();
     }
-
 
     public function getLimiterTimeWrapper() {
         return $this->limiterWrapper;
     }
 
-
-    public function testFlow() {
-        $this->doTheLimiterFlow();
-    }
-//put your code here
 }
